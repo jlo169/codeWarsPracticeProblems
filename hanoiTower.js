769516c57c3f07f37dd2e6d45306450525c2b800
Jonathan <kakashi169@gmail.com>
2019-10-03 11:08:42 +0000

    started tower of hanoi planning, need more research on recursion to work

---
+++ hanoiTower.js
@@ -0,0 +1,10 @@
+// [[1,2,3], [], []] --> [[], [], [1,2,3]];
+
+
+const towerOfHanoi = arr => {
+    const puzzle = [[], []];
+    puzzle.unshift(arr);
+    while (puzzle)
+}
+
+console.log(towerOfHanoi([1,2,3]));
