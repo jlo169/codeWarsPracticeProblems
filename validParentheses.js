cddaf12739069d5d5dfd2f544593e2f180c8608b
Jonathan <kakashi169@gmail.com>
2020-03-11 06:40:24 +0000

    added more optimized solution found

--- validParentheses.js
+++ validParentheses.js
@@ -53,3 +53,34 @@ var isValid = function(s) {
     return true;
   }
 };
+
+// if (s.length === 0) return true
+// if (s.length === 1) return false
+// if (s.length % 2 !== 0) return false
+
+// const dictionary = {
+// 	'}': '{',
+// 	')': '(',
+// 	']': '['
+// }
+// const stack = []
+
+// for (let i = 0; i < s.length; i++) {
+// 	const currChar = s[i]
+// 	const lastChar = stack[stack.length - 1]
+// 	const delChar = dictionary[currChar]
+
+// 	if (delChar) {
+// 		// 2
+// 		if (delChar === lastChar) {
+// 			stack.pop()
+// 		} else {
+// 			return false
+// 		}
+// 	} else {
+// 		stack.push(currChar)
+// 	}
+// }
+
+// // 3
+// return !stack.length
